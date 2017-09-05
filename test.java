4f0d3525c5bc79c94475be7a486fbe7d27384ea4
yukuaifeng <yukuaifeng@hotmail.com>
2017-09-05 16:46:44 +0800

    添加了一个java文件

---
+++ test.java
@@ -0,0 +1,6 @@
+public class test{
+    public static void main(String[] args){
+        System.out.println("This is the first java file");
+        System.out.println("Now i will add a line");
+    }
+}
