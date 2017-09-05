6d4e8094f467fa5459022acad04b1f33fb51cc1a
yukuaifeng <yukuaifeng@hotmail.com>
2017-09-05 16:54:05 +0800

    try again

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test{
     public static void main(String[] args){
         System.out.println("This is the first java file");
         System.out.println("Now i will add a line");
+        System.out.println("this is the first java file");
     }
 }
